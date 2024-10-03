9e4d7cfd88c5dec3c976d511bac0b2e259452cba
Alexa Bedecarratz <abedecarratz@uc.cl>
2024-10-03 12:44:24 -0300

    Initial setup for frontend with Vite and React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Hello World from Frontend!</h1>
+      <p>This is a simple React app created with Vite.</p>
+    </div>
+  )
+}
+
+export default App
