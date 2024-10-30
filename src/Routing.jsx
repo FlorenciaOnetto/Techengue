7db744fd9885505bbf72adfa1e2c6149766c31d3
Dominga Browne <domingabrowner@gmail.com>
2024-10-30 13:36:18 -0300

    Agrega vista de reseña

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import App from './App'
-import PaginaInicio from "./PaginaInicio"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './App';
+import PaginaInicio from "./PaginaInicio";
+import ResenaForm from "./ResenaForm"; // Importa el componente de reseña
 
 export default function Routing() {
-
     return (
         <>
         <BrowserRouter>
             <Routes>
-                <Route path={'/inicio'} element={<PaginaInicio/>}/>
-                <Route path={'/'} element={<App/>}/>
+                <Route path="/inicio" element={<PaginaInicio />} />
+                <Route path="/resena" element={<ResenaForm />} /> {/* Nueva ruta para la vista de reseña */}
+                <Route path="/" element={<App />} />
             </Routes>
         </BrowserRouter>
         </>
-    )
+    );
 }
