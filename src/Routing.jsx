3688c13ea318a3b1cbd932ba88e3ec402d701c7c
FlorenciaOnetto <florencia.onetto@uc.cl>
2024-10-29 21:26:00 -0300

    arreglos ame

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from './App'
 import PaginaInicio from "./PaginaInicio"
+import AdoptionRequestForm from "./SolicitudAdopcion"
+import PetAdoptionRequests from "./Solicitudes"
 
 export default function Routing() {
 
@@ -10,6 +12,8 @@ export default function Routing() {
             <Routes>
                 <Route path={'/inicio'} element={<PaginaInicio/>}/>
                 <Route path={'/'} element={<App/>}/>
+                <Route path={"/SolicitudAdopcion"} element={<AdoptionRequestForm/>} />
+                <Route path={"/Solicitudes"} element={<PetAdoptionRequests/>} />
             </Routes>
         </BrowserRouter>
         </>
