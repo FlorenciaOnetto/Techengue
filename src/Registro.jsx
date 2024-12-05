cd25d96c009ea82e4281ddaafe370077ceb08d8a
Alexa Bedecarratz <abedecarratz@uc.cl>
2024-12-05 15:53:34 -0300

    vercel

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -10,7 +10,7 @@ function Registro() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/auth/register', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nombre, email, password })
